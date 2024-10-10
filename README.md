05cc5bfb1cc6646730228e124d6ab04f3da7ed65
esterwalf <estellewalford@gmail.com>
2024-10-10 20:04:05 +0100

    solution for finding the unique nunber

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # codewars
-https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/python
+https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/python
---
+++ school_paperwork.py
@@ -0,0 +1,24 @@
+"""Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+Example:
+n= 5, m=5: 25
+n=-5, m=5:  0
+Waiting for translations and Feedback! Thanks!"""
+def paperwork(n, m):
+    # Happy Coding! ^_^
+
+"""   import codewars_test as test
+    from solution import paperwork
+
+    @test.describe("Fixed Tests")
+    def basic_tests():
+        @test.it('Basic Test Cases')
+        def basic_test_cases():
+            test.assert_equals(paperwork(5, 5), 25, "Failed at Paperwork(5,5)")
+            test.assert_equals(paperwork(1, 2), 2, "Failed at Paperwork(1,2)")
+            test.assert_equals(paperwork(-5, 5), 0, "Failed at Paperwork(-5,5)")
+            test.assert_equals(paperwork(5, -5), 0, "Failed at Paperwork(5,-5)")
+            test.assert_equals(paperwork(-5, -5), 0, "Failed at Paperwork(-5,-5)")
+            test.assert_equals(paperwork(5, 0), 0, "Failed at Paperwork(5,0)")"""
