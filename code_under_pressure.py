5404f82ac57ae251476435e1c23b7646a791ea5d
esterwalf <estellewalford@gmail.com>
2024-10-10 19:42:11 +0100

    solution 1 doubling an integer

--- code_under_pressure.py
+++ code_under_pressure.py
@@ -1,3 +1,15 @@
+#Code as fast as you can! You need to double the integer and return it.
 def double_integer(i):
     return i*2 # Double the integer and return it!
 
+"""import codewars_test as test
+
+@test.describe('Tests')
+def tests():
+    @test.it('Sample Test Case')
+    def sample_test_case():
+        test.assert_equals(double_integer(2), 4);
+        test.assert_equals(double_integer(4), 8);
+        test.assert_equals(double_integer(-10), -20);
+        test.assert_equals(double_integer(0), 0);
+        test.assert_equals(double_integer(100), 200);"""
---
+++ find_the_uniquw_number.py
@@ -0,0 +1,31 @@
+"""There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+
+This is the first kata in series:
+
+Find the unique number (this kata)
+Find the unique string
+Find The Unique"""
+
+def find_uniq(arr):
+    # your code here
+    return n   # n: unique number in the array
+
+"""try:
+    import codewars_test as test
+except:
+    pass
+
+
+@test.describe("Basic Tests")
+def f():
+    @test.it("Simple tests")
+    def _():
+        test.assert_equals(find_uniq([ 1, 1, 1, 2, 1, 1 ]), 2)
+        test.assert_equals(find_uniq([ 0, 0, 0.55, 0, 0 ]), 0.55)
+        test.assert_equals(find_uniq([ 3, 10, 3, 3, 3 ]), 10)"""
